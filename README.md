438901abe4cb267d4d3cf0e13d82b3f103ab2b6b
divperedi <132815746+divperedi@users.noreply.github.com>
2024-04-28 13:19:11 +0200

    crash course done

---
+++ README.md
@@ -0,0 +1,1 @@
+# Following Traversy Media's Styled Components Crash Course & Project
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import { Button } from './styles/Button.styled';
+import { Flex } from './styles/Flex.styled';
+
+function Header() {
+    return (
+        <StyledHeader>
+            <Container>
+                <Nav>
+                    <Logo src='./images/logo.svg' alt='logo' />
+                    <Button>Try it free</Button>
+                </Nav>
+
+                <Flex>
+                    <div>
+                        <h1>Biuld the community your fans will love</h1>
+                        <p>
+                            Huddle re-imagines the way we build communities. You
+                            have a voice,
+                            but so does youe audience. Create connections with your
+                            users as
+                            you engage in genuine discussion.
+                        </p>
+                        <Button bg='#ff0099' color='#fff'>
+                            Get started for free
+                        </Button>
+                    </div>
+                    <Image src='./images/illustration-mockups.svg' alt='' />
+                </Flex>
+            </Container>
+        </StyledHeader>
+    )
+}
+
+export default Header
